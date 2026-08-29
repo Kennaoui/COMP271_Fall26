743a61a7ad05451cfe277ca5f621bce5b0b43100
Kennaoui <kennaoui@luc.edu>
2026-08-29 13:13:52 -0500

    Create letters.py

---
+++ W2/In_Class_Activity/letters.py
@@ -0,0 +1,61 @@
+"""Block-letter data used by the horizontal river activity."""
+
+M = [
+    " M       M",
+    " M M   M M",
+    " M  M M  M",
+    " M   M   M",
+    " M       M",
+    " M       M",
+    " M       M",
+]
+
+I = [
+    "  I ",
+    "  I ",
+    "  I ",
+    "  I ",
+    "  I ",
+    "  I ",
+    "  I ",
+]
+
+S = [
+    "   SSSSSSS",
+    " SS       ",
+    " SS       ",
+    "  SSSSSS  ",
+    "        SS",
+    "        SS",
+    " SSSSSSS  ",
+]
+
+P = [
+    " PPPPPPPP ",
+    " P      PP",
+    " P     PP ",
+    " PPPPPP   ",
+    " P        ",
+    " P        ",
+    " P        ",
+]
+
+
+def sanity_check(letters):
+    """Return True when all letters are nonempty and have the same height."""
+    if len(letters) == 0 or len(letters[0]) == 0:
+        return False
+
+    expected_height = len(letters[0])
+
+    for letter in letters:
+        if len(letter) != expected_height:
+            return False
+
+    return True
+
+
+MISSISSIPPI = [M, I, S, S, I, S, S, I, P, P, I]
+
+# Catch malformed letter data before the activity begins.
+assert sanity_check(MISSISSIPPI)
