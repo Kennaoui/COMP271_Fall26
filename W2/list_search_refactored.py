941fa20fa8ca15bd7f2b5e68e2e3f3edba310116
Kennaoui <kennaoui@luc.edu>
2026-09-04 19:53:13 -0500

    Refactor list search functions
    Refactor indices_of function to improve clarity and add helper functions for specific use cases.

--- W2/list_search_refactored.py
+++ W2/list_search_refactored.py
@@ -1,7 +1,6 @@
 def indices_of(list_to_search: list, item_to_find: str, find_all: bool) -> int | list[int]:
     """
-    Return all matching indices when all is True.
-    Otherwise, return the first matching index or -1 if not found.
+    Return all matching indices when find_all is True. Else, return the first matching index or -1 if not found.
     """
     # Results will be stored in indices. The function will decide
     # what to return at the end based on the value of all.
@@ -15,7 +14,7 @@ def indices_of(list_to_search: list, item_to_find: str, find_all: bool) -> int |
             indices.append(i)
 
             # Exit early if we are interested only in the first occurrence.
-            if not all:
+            if not find_all:
                 i = len(list_to_search)
 
         i += 1
@@ -23,7 +22,7 @@ def indices_of(list_to_search: list, item_to_find: str, find_all: bool) -> int |
     # Decide what to return.
     result: int | list[int] = indices
 
-    if not all:
+    if not find_all:
         # We are interested only in the first occurrence.
         if len(indices) == 0:
             # No occurrence found.
@@ -32,3 +31,15 @@ def indices_of(list_to_search: list, item_to_find: str, find_all: bool) -> int |
             result = indices[0]
 
     return result
+    
+def index_of_all(list_to_search: list[str], item_to_find: str) -> list[int]:
+    """Returns list of indices where item_to_find is found in list_to_search, or empty list if not found."""
+    return indices_of(list_to_search, item_to_find, True)
+
+def index_of(list_to_search: list[str], item_to_find: str) -> int:
+    """Returns the index of item_to_find in list_to_search, or -1 if not found.""" 
+    return indices_of(list_to_search, item_to_find, False)
+
+def contains(list_to_search: list[str], item_to_find: str) -> bool:
+    """Returns True if item_to_find is in list_to_search, False otherwise."""
+    return index_of(list_to_search, item_to_find) != -1
